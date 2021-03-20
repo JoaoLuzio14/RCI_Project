4fee3245dc609819fb9c4aff140c141d376b2aaa
Joao Luzio <joaoluzio14@tecnico.ulisboa.pt>
2021-03-20 19:10:25 +0000

    Arguments V2

--- ndn.c
+++ ndn.c
@@ -1,5 +1,6 @@
 /* Comment Header */
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -16,14 +17,13 @@ int check_ip(char *full_ip);
 int main(int argc, char **argv){
 
 	/* Common Variables */
-	int i;
+	int i, errcode;
 	char argvector[5][8] = {"Name", "IP", "TCP", "regIP", "regUDP"}; // Desired Arguments Description
-	char str_msg[] = "REG 19 9.9.9.9 9"
 
 	/* UDP Server Variables */
 	struct addrinfo hints, *res;
 	struct sockaddr addr;
-	int fd, errcode;
+	int fd;
 	ssize_t n;
 	socklen_t addrlen;
 	char buffer[128+1];
@@ -35,6 +35,7 @@ int main(int argc, char **argv){
 	for(i=0; i<argc; i++){
 		printf("%s: %s\n", argvector[i], argv[i]);
 	}
+	/*
 	if(argc < 3){
 		printf("Invalid number of arguments. Very few arguments inserted.\nThe ideal executable command is: 'ndn IP TCP regIP regUDP'\n");
 		exit(1);
@@ -52,6 +53,7 @@ int main(int argc, char **argv){
 		printf("Valid number of arguments.\n");
 		printf("Arguments are valid.\n");
 	}
+	*/
 
 	/* UDP Node Server Connection Setup */
 
@@ -62,19 +64,22 @@ int main(int argc, char **argv){
 	hints.ai_family = AF_INET; // IPv4
 	hints.ai_socktype = SOCK_DGRAM; // UDP socket
 
-	errcode = getaddrinfo(UDP_IP, UDP_PORT, &hints, &res);
-	if(errcode!=0) exit(1);
+	errcode = getaddrinfo("139.136.138.142", "59000", &hints, &res);
+	if(errcode != 0) exit(1);
 	
 	/* User Interface */
-	while(1){
 
-		n = sendto(fd, str_msg, strlen(str_msg), 0, res->ai_addr, res->ai_addrlen);
+	while(1){
+		//REG node
+		printf("Estou Aqui!\n");
+		n = sendto(fd, "REG 19 9.9.9.9 9", strlen("REG 19 9.9.9.9 9"), 0, res->ai_addr, res->ai_addrlen);
 		if(n == -1) exit(1);
 
 		addrlen = sizeof(addr);
 		n = recvfrom(fd, buffer, 128, 0, &addr, &addrlen);
-		if(n==-1) exit(1);
-		buffer[n] = ‘\0’;
+		printf("Chega aqui!\n");
+		if(n == -1) exit(1);
+		buffer[n] = '\0';
 		printf("%s\n", buffer);
 
 		break;
@@ -88,8 +93,6 @@ int main(int argc, char **argv){
 }
 
 int check_ip(char *full_ip){
-
-
 
 	return 0;
 }
